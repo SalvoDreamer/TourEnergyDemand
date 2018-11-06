e97876500655e386416e2425a751bec5c2e397fa
Salvatore Cantoro <salvatore.cantoro@finconsgroup.local>
2018-11-06 12:01:45 +0100

    Primo Commit - Importazione Progetto

---
+++ mobile/src/main/java/it/politecnico_di_bari/sorbo_cantoro/tourenergydemand/login.java
@@ -0,0 +1,13 @@
+package it.politecnico_di_bari.sorbo_cantoro.tourenergydemand;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
