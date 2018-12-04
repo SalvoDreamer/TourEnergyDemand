655f3c145b39c8a70cb81eef2cec32ad917cde38
SalvoDreamer <salvatore.cantoro@hotmail.it>
2018-12-04 23:27:59 +0100

    importing maps libraries

--- mobile/src/main/java/it/politecnico_di_bari/sorbo_cantoro/tourenergydemand/HomepageTED.java
+++ mobile/src/main/java/it/politecnico_di_bari/sorbo_cantoro/tourenergydemand/HomepageTED.java
@@ -1,13 +1,27 @@
 package it.politecnico_di_bari.sorbo_cantoro.tourenergydemand;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomepageTED extends AppCompatActivity {
+
+    private Button mapButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage_ted);
+
+        mapButton = (Button) findViewById(R.id.btnMaps);
+
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomepageTED.this,Mappa.class));
+            }
+        });
     }
 }
